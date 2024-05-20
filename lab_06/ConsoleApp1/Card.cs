9994669e4bea8bdbbe508f5a4e63ab6caf134906
Marian Tsyvinskyi <mariantop228@gmail.com>
2024-05-20 13:41:21 +0300

    Implemented equality checks in the Card class to ensure cards can be compared.

--- lab_06/ConsoleApp1/Card.cs
+++ lab_06/ConsoleApp1/Card.cs
@@ -1,4 +1,6 @@
 ﻿// src/Card.cs
+using System;
+
 namespace ATMProject
 {
     public class Card
@@ -7,7 +9,17 @@ namespace ATMProject
 
         public Card(string cardNumber)
         {
-            CardNumber = cardNumber;
+            CardNumber = cardNumber ?? throw new ArgumentNullException(nameof(cardNumber));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Card card && CardNumber == card.CardNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return CardNumber.GetHashCode();
         }
     }
 }
